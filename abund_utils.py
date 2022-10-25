88ddc40717cd08993631726b6142ec02d382afe1
Christian Hayes <chrishayesastro@gmail.com>
2022-10-25 14:21:02 -0700

    Added Grevesse et al. 2007 solar abundances

--- abund_utils.py
+++ abund_utils.py
@@ -258,6 +258,8 @@ ion_numbers = {
 def solar_abund(reference="asplund2009"):
     a05 = ['asplund2005', 'asplund_2005', 'a05', 'a_05']
     a09 = ['asplund2009', 'asplund_2009', 'a09', 'a_09']
+    g07 = ['grevesse2007', 'grev2007', 'g07',
+           'g_07', 'grev07', 'gre07', 'gre2007']
 
     if reference.lower() in a05:
         solar_abu = {
@@ -335,6 +337,92 @@ def solar_abund(reference="asplund2009"):
             'W': 1.11,
             'Re': 0.23,  # Meteoric abundances
             'Os': 1.45,
+            'Ir': 1.38,
+            'Pt': 1.64,  # Meteoric abundances
+            'Au': 1.01,
+            'Hg': 1.13,  # Meteoric abundances
+            'Tl': 0.90,
+            'Pb': 2.00,
+            'Bi': 0.65,  # Meteoric abundances
+            'Th': 0.06,  # Meteoric abundances
+            'U': -0.52  # Meteoric abundances
+        }
+    elif reference.lower() in g07:
+        solar_abu = {
+            'H': 12.00,
+            'He': 10.93,  # Indirect estimate
+            'Li': 1.05,
+            'Be': 1.38,
+            'B': 2.70,
+            'C': 8.39,
+            'N': 7.78,
+            'O': 8.66,
+            'F': 4.56,
+            'Ne': 7.84,  # Indirect estimate
+            'Na': 6.17,
+            'Mg': 7.53,
+            'Al': 6.37,
+            'Si': 7.51,
+            'P': 5.36,
+            'S': 7.14,
+            'Cl': 5.50,
+            'Ar': 6.18,  # Indirect estimate
+            'K': 5.08,
+            'Ca': 6.31,
+            'Sc': 3.17,
+            'Ti': 4.90,
+            'V': 4.00,
+            'Cr': 5.64,
+            'Mn': 5.39,
+            'Fe': 7.45,
+            'Co': 4.92,
+            'Ni': 6.23,
+            'Cu': 4.21,
+            'Zn': 4.60,
+            'Ga': 2.88,
+            'Ge': 3.58,
+            'As': 2.29,  # Meteoric abundances
+            'Se': 3.33,  # Meteoric abundances
+            'Br': 2.56,  # Meteoric abundances
+            'Kr': 3.25,  # Indirect estimate
+            'Rb': 2.60,
+            'Sr': 2.92,
+            'Y': 2.21,
+            'Zr': 2.58,
+            'Nb': 1.42,
+            'Mo': 1.92,
+            'Ru': 1.84,
+            'Rh': 1.12,
+            'Pd': 1.66,
+            'Ag': 0.94,
+            'Cd': 1.77,
+            'In': 1.60,
+            'Sn': 2.00,
+            'Sb': 1.00,
+            'Te': 2.19,  # Meteoric abundances
+            'I': 1.51,  # Meteoric abundances
+            'Xe': 2.24,  # Indirect estimate
+            'Cs': 1.07,  # Meteoric abundances
+            'Ba': 2.17,
+            'La': 1.13,
+            'Ce': 1.70,
+            'Pr': 0.58,
+            'Nd': 1.45,
+            'Sm': 1.00,
+            'Eu': 0.52,
+            'Gd': 1.11,
+            'Tb': 0.28,
+            'Dy': 1.14,
+            'Ho': 0.51,
+            'Er': 0.93,
+            'Tm': 0.00,
+            'Yb': 1.08,
+            'Lu': 0.06,
+            'Hf': 0.88,
+            'Ta': -0.17,  # Meteoric abundances
+            'W': 1.11,
+            'Re': 0.23,  # Meteoric abundances
+            'Os': 1.25,
             'Ir': 1.38,
             'Pt': 1.64,  # Meteoric abundances
             'Au': 1.01,
